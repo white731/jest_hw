07029c4895e704499f85cf2292dbe5c95d018bd2
Landon Whitesides <whitesideslandon@gmail.com>
2020-12-02 16:29:40 -0700

    finished assignment working on bonus

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,18 @@
 import {
   // methods,
   // methods,
-  originalArray
+  originalArray,
+  doubleArray,
+  evensOnlyArray,
+  arraySum,
+  allNumbersGreaterThanZero,
+  someNumbersAreOdd,
+  evensOnlyAndDoubleArray,
+  findItem,
+  sortArray,
+  sortArrayOfLetters,
+  sortArrayOfLettersInReverse,
+  doubleLikes
 } from '../app'
 
 // hint of look in before_each before_all
@@ -10,3 +21,111 @@ describe('originalArray Test', ()=> {
     expect(originalArray([1,2,3,4])).toEqual([1,2,3,4])
   })
 })
+
+describe("Double Array",()=>{
+    test("double array with just normal numbers", ()=>{
+      let numberArray = [1,2,3,4]
+      let result = doubleArray(numberArray)
+      expect(result).toEqual([2,4,6,8])
+    })
+})
+
+describe("Check for evens in array",()=>{
+  test("array with just normal incrementing numbers", ()=>{
+    let numberArray = [1,2,3,4]
+    let result = evensOnlyArray(numberArray)
+    expect(result).toEqual([2,4])
+  })
+})
+
+describe("Sum up an array",()=>{
+  test("array with just normal incrementing numbers", ()=>{
+    let numberArray = [1,2,3,4]
+    let result = arraySum(numberArray)
+    expect(result).toEqual(10)
+  })
+})
+
+describe("Test to see if all numbers in an array are greater than 0",()=>{
+  test("array with just normal incrementing numbers", ()=>{
+    let numberArray = [1,2,3,4]
+    let result = allNumbersGreaterThanZero(numberArray)
+    expect(result).toEqual(true)
+  })
+})
+
+describe("Test to see any of the numbers in an array are odd",()=>{
+  test("array with just normal incrementing numbers", ()=>{
+    let numberArray = [1,2,3,4]
+    let result = someNumbersAreOdd(numberArray)
+    expect(result).toEqual(true)
+  })
+})
+
+describe("Numbers are doubled and show only even ",()=>{
+  test("array with just normal incrementing numbers", ()=>{
+    let numberArray = [1,2,3,4]
+    let result = evensOnlyAndDoubleArray(numberArray)
+    expect(result).toEqual([4,8])
+  })
+})
+
+describe("Find a number within an array ",()=>{
+  test("array with just normal incrementing numbers", ()=>{
+    let numberArray = [1,2,3,4]
+    let find_num = 1
+    let result = findItem(numberArray,find_num)
+    expect(result).toEqual(1)
+  })
+
+  test("try to find a number that doesn't exist in the array", ()=>{
+    let numberArray = [1,2,3,4]
+    let find_num = 7
+    let result = findItem(numberArray,find_num)
+    expect(result).toEqual(1)
+  })
+})
+
+describe("Sort an array of anything",()=>{
+  test("array with just normal incrementing numbers", ()=>{
+    let numberArray = [1,2,3,4]
+    let result = sortArray(numberArray)
+    expect(result).toEqual([4,3,2,1])
+  })
+  test("array with random numbers", ()=>{
+    let numberArray = [4,5,10,3,1,7]
+    let result = sortArray(numberArray)
+    expect(result).toEqual([10,7,5,4,3,1])
+  })
+  test("array with letters", ()=>{
+    let numberArray = ["z","x","b","q"]
+    let result = sortArrayOfLetters(numberArray)
+    console.log(result)
+    expect(result).toEqual(["b","q","x","z"])
+  })
+  test("array with letters going backwards", ()=>{
+    let numberArray = ["z","x","b","q"]
+    let result = sortArrayOfLettersInReverse(numberArray)
+    console.log(result)
+    expect(result).toEqual(["z","x","q","b"])
+  })
+})
+
+describe("Double the likes of all artists",()=>{
+  test("array of objects", ()=>{
+    let artists = [
+    {name: 'a', likes:100},
+    {name: 'b', likes:99},
+    {name: 'c', likes:101}
+  ]
+
+    let result = doubleLikes(artists)
+    expect(result).toEqual([
+         {name: 'a', likes:200},
+         {name: 'b', likes:198},
+         {name: 'c', likes:202},  
+      ])
+  })
+})
+
+
--- app.js
+++ app.js
@@ -1,3 +1,110 @@
+// EXPORT ALL AS FUNCTIONS AND KEEP THE NAMES THE SAME
+
+// ways to export
+
+// export const methodName = (param) => {
+// }
+
+// ----or---------
+
+// export methodName function(param){
+// }
+
+// Already Provied Example
+
 export const originalArray = (arr) =>{
   return arr
 }
+
+// ARRAY ITERATOR METHODS
+
+// numberArray = [1,2,3,4]
+
+// return array with double values in array
+export const doubleArray = (numberArray) => {
+  return numberArray.map((x)=>x*2)
+}// => [2,4,6,8];  hint use map 
+
+// return array with all even numbers
+export const evensOnlyArray = (numberArray) => {
+    return numberArray.filter( x => x % 2 == 0)
+} // => [2,4];  hint use filter 
+
+// return array with sum of numbers
+export const arraySum = (numberArray) => {
+  return numberArray.reduce((x, y) => x + y)
+
+} // => 10;  hint use reduce
+
+// return true if every item greater than 0
+export const allNumbersGreaterThanZero = (numberArray) => {
+  return numberArray.every((x)=> x > 0)
+}// => true; use every
+
+// return true if one or more items in array are odd
+export const someNumbersAreOdd = (numberArray) => {
+  return numberArray.some(x => x % 2 != 0)
+
+}// => true; use some
+
+// return array double and even
+export const evensOnlyAndDoubleArray = (numberArray) => {
+  return numberArray.filter(x => x % 2 == 0).map(x => x*2)
+
+} // => [4,8]; // use map and filter
+
+// find an item method return 'not found' if not found
+export const findItem = (numberArray, numToFind) => {
+  return numberArray.find( x => x == numToFind ? x : "not found")
+} //=> 1
+
+// sort array desc order
+export const sortArray = (numberArray) => {
+  return numberArray.sort((a,b) => b-a)
+} //=> [4,3,2,1] *desc*
+
+export const sortArrayOfLetters = (numberArray) => {
+  return numberArray.sort()
+} //=> [4,3,2,1] *desc*
+
+export const sortArrayOfLettersInReverse = (numberArray) => {
+  return numberArray.sort().reverse()
+} //=> [4,3,2,1] *desc*
+
+
+// -----BONUS------
+// ARRAY ITERATOR METHODS ON Objects with name age 
+
+// artists = [
+//   {name: 'a', likes:100},
+//   {name: 'b', likes:99},
+//   {name: 'c', likes:101}, 
+// ]
+// doubles likes of artist
+
+export const doubleLikes = (artists) => {
+  return artists.map((artist) => (
+    {name: artist.name, likes: artist.likes * 2}
+  ))  
+}
+// => [
+//   {name: 'a', likes:200},
+//   {name: 'b', likes:198},
+//   {name: 'c', likes:202},  
+// ]; 
+
+export const moreThan100Likes = (artists) => {
+  return artists.filter(artist => artist.likes > 100)
+}// =>[ {name: 'c', likes:101} ]; // use filter
+
+// //  return an array of strings of artist name
+// justArtistMoreThan100Likes(artists) //=> ['c']; // use filter
+
+// // get total number of likes fo all artists
+// numberOfLikes(artists) //=> 300; // get all likes
+
+// // find an item method. Returns 'not found' if not found
+// findObjByName(artists, 'a') // => {name: 'a', likes:200}
+
+// // returns array of artist sorted by likes
+// sortArtistByName(artists)
